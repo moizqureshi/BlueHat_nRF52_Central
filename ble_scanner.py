e04852ed5e66d98f856062e735028987b9f6c46d
Moiz Qureshi <moquresh@ucsd.edu>
2017-05-13 22:08:12 -0700

    added connection to bluehat socketio server

--- ble_scanner.py
+++ ble_scanner.py
@@ -1,9 +1,46 @@
 from bluepy.btle import Scanner, DefaultDelegate
+from apscheduler.schedulers.background import BlockingScheduler
+from socketIO_client import SocketIO, LoggingNamespace, BaseNamespace
+import logging
+import requests
+import json
+import datetime
 
-scanner = Scanner()
-devices = scanner.scan(5.0)
+OBSERVER_ID = "1"
+OBSERVER_LOCATION = "Location 1"
 
-for dev in devices:
-    if (dev.scanData.get(255, None).encode("hex")[:28] == "ffff0123456789abcdefffffffff"):
-        print "Device ID: %s, RSSI: %d dB" % (dev.scanData.get(255, None).encode("hex")[29:36], dev.rssi)
-        print "BlueHat Data: %s" % dev.scanData.get(255, None).encode("hex")[37:]
+class BlueHatObserver(BaseNamespace):
+    def on_connect(self):
+        print('[Connected]')
+
+    def on_reconnect(self):
+        print('[Reconnected]')
+
+    def on_disconnect(self):
+        print('[Disconnected]')
+
+    def handleMessage(self):
+        print('observer_json_msg', args)
+
+
+def scheduledAdvertiserScan():
+    devices = scanner.scan(3.0)
+    print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    for dev in devices:
+        if (dev.scanData.get(255, None).encode("hex")[:28] == "ffff0123456789abcdefffffffff"):
+            print "Device ID: %s, RSSI: %d dB" % (dev.scanData.get(255, None).encode("hex")[29:36], dev.rssi)
+            print "BlueHat Data: %s" % dev.scanData.get(255, None).encode("hex")[37:]
+    print "\n"
+
+
+if __name__ == "__main__":
+    socketIO = SocketIO('http://e41f2ed7.ngrok.io')
+    bluehatObserver_socket = socketIO.define(BlueHatObserver, '/BlueHatObserver')
+    bluehatObserver_socket.emit('observer_json_msg', {'xxx': 'yyy'})
+    logging.basicConfig()
+    scanner = Scanner()
+
+    print 'Starting Observer Scan Scheduler!\n'
+    scheduler = BlockingScheduler()
+    scheduler.add_job(scheduledAdvertiserScan, 'interval', seconds = 5, id = 'advertiserScan', misfire_grace_time = 1)
+    scheduler.start()
