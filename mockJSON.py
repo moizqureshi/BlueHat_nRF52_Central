080b4613c11799438e85b9cc5b10393dbc0590a3
Moiz Qureshi <moizq@calpak-usa.com>
2017-05-17 19:11:28 -0700

    fully working test with emit, response, and callback print out

--- mockJSON.py
+++ mockJSON.py
@@ -1,25 +1,39 @@
 from apscheduler.schedulers.background import BlockingScheduler
 from socketIO_client import SocketIO, LoggingNamespace, BaseNamespace
-from execjs import get
 import os
 import logging
 import requests
 import json
 import datetime
 
+# logging.getLogger('requests').setLevel(logging.WARNING)
+# logging.basicConfig(level=logging.DEBUG)
 
 OBSERVER_ID = "1"
 OBSERVER_LOCATION = "Location 1"
+HOST = 'http://127.0.0.1/'
+PORT = 8000
+NAMESPACE = "/observer"
 
 class Observer(BaseNamespace):
     def on_connect(self):
-        print('Device: Connected to BlueHat Server')
+        print('Device: Connected from BlueHat Server')
 
     def on_disconnect(self):
         print('Device: Disconnected from BlueHat Server')
 
-def on_server_response(self, *args):
+    def on_server_response(self, *args):
+        print('BlueHat Server Response: ', args)
+
+def on_server_response(*args):
     print('BlueHat Server Response: ', args)
+
+
+socketIO = SocketIO(HOST, PORT)
+observer = socketIO.define(Observer, NAMESPACE)
+observer.on('on_server_response', on_server_response)
+socketIO.wait(seconds=1)
+
 
 def scheduledAdvertiserScan():
     print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -32,22 +46,12 @@ def scheduledAdvertiserScan():
     }
     print "Device ID: %s, RSSI: %d dB" % (json_data['advertiser_id'], json_data['rssi'])
     print "BlueHat Data: %s" % json_data['data']
-    socketIO.emit('observer_json_msg', json_data)
-    socketIO.wait(seconds=0.25)
+    observer.emit('observer_json_msg', json_data)
+    socketIO.wait(seconds=1)
     print('\n')
 
 
 if __name__ == "__main__":
-    logging.getLogger('requests').setLevel(logging.WARNING)
-    logging.basicConfig(level=logging.DEBUG)
-
-    print('Connecting to BlueHat SocketIO Server')
-    socketIO = SocketIO('http://127.0.0.1', 5000, Observer)
-    socketIO = socketIO.define(Observer, '/Observer')
-    # socketIO.on('connect', thisObserver.on_connect)
-    # socketIO.on('disconnect', thisObserver.on_disconnect)
-    socketIO.on('observer_json_msg', on_server_response)
-
     print 'Starting Observer Scan Scheduler!\n'
     scheduler = BlockingScheduler()
     scheduler.add_job(scheduledAdvertiserScan, 'interval', seconds = 3, id = 'advertiserScan', misfire_grace_time = 1)
--- socketio_client.js
+++ socketio_client.js
@@ -1,6 +1,6 @@
 //client.js
 var io = require('socket.io-client');
-var socket = io.connect('http://localhost:5000', {reconnect: true});
+var socket = io.connect('http://localhost:8000/Observer', {reconnect: true});
 
 // Add a connect listener
 socket.on('connect', function (socket) {
