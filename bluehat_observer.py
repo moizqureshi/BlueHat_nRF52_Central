f301609270960d0e79779d0999d37ccf84f4435e
Moiz Qureshi <moquresh@ucsd.edu>
2017-06-07 04:25:40 -0700

    last fix for this script

--- bluehat_observer.py
+++ bluehat_observer.py
@@ -12,12 +12,11 @@ logging.basicConfig()
 # logging.getLogger('requests').setLevel(logging.WARNING)
 # logging.basicConfig(level=logging.DEBUG)
 
-HOST = 'http://e14b64f1.ngrok.io'
+HOST = 'http://192.168.1.152:8000'
 PORT = 8000
 NAMESPACE = "/observer"
 
 OBSERVER_ID = "1"
-OBSERVER_LOCATION = "Location 1"
 
 class Observer(BaseNamespace):
     def on_connect(self):
@@ -32,19 +31,19 @@ def on_server_response(*args):
 
 def emitWorker(json_list):
     observer.emit('observer_json_msg', json.dumps(json_list))
-    socketIO.wait(seconds=0.5)
+    # socketIO.wait(seconds=0.1)
 
 
 socketIO = SocketIO(HOST, None)
 observer = socketIO.define(Observer, NAMESPACE)
-observer.on('on_server_response', on_server_response)
+# observer.on('on_server_response', on_server_response)
 socketIO.wait(seconds=1)
 
 scanner = Scanner()
 
 
 def scheduledAdvertiserScan():
-    devices = scanner.scan(3.0)
+    devices = scanner.scan(0.5)
     print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     json_list = []
     for dev in devices:
@@ -58,11 +57,10 @@ def scheduledAdvertiserScan():
                 rssi = dev.rssi
                 data = dev.scanData.get(255, None).encode("hex")[37:]
                 json_data = {
-                    "ObserverID":OBSERVER_ID,
-                    "ObserverLocation":OBSERVER_LOCATION,
-                    "DeviceID":deviceID,
-                    "RSSI":rssi,
-                    "Data":data
+                    "ObserverID": OBSERVER_ID,
+                    "DeviceID": deviceID,
+                    "RSSI": rssi,
+                    "Data": data
                 }
                 print "Device ID: %s, RSSI: %d dB" % (deviceID, rssi)
                 print "BlueHat Data: %s" % data
@@ -73,5 +71,5 @@ if __name__ == "__main__":
 
     print 'Starting Observer Scan Scheduler!\n'
     scheduler = BlockingScheduler()
-    scheduler.add_job(scheduledAdvertiserScan, 'interval', seconds = 5, id = 'advertiserScan', misfire_grace_time = 1)
+    scheduler.add_job(scheduledAdvertiserScan, 'interval', seconds=1.5, id='advertiserScan', misfire_grace_time=1)
     scheduler.start()
